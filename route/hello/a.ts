467ea7d2f744ede492ec16883cd58a01fa17007a
zhangyu <598586050@qq.com>
2023-10-23 23:23:47 +0800

    feature:【阶段性开发】

--- route/hello/a.ts
+++ route/hello/a.ts
@@ -1,11 +1,10 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-18 10:49:20
- * @LastEditTime: 2023-10-22 13:38:46
+ * @LastEditTime: 2023-10-21 19:47:29
  */
 import { RouteType } from 'think-ts-lib'
 
 export default (route: RouteType) => {
-    route.get('/hello', 'a/')
-    //route.get('/hello', 'sdf')
+    route.get('/b', 'sdf')
 }
