e0d1cad55ebc3d71c1fa3d59b0605b95b43277fc
zhangyu <598586050@qq.com>
2023-11-07 08:43:11 +0800

    feature:【阶段提交】

--- controller/a/hello.ts
+++ controller/a/hello.ts
@@ -1,23 +1,24 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-24 10:04:07
- * @LastEditTime: 2023-10-25 11:28:53
+ * @LastEditTime: 2023-10-28 16:33:37
  */
 import { Context, Controller } from 'think-ts-lib'
 
 export default class HelloController extends Controller {
-    async say(){
-        return {
-            body: `<!DOCTYPE html>
+  async say() {
+    // return View('/index', {count: 1})
+    return {
+      body: `<!DOCTYPE html>
             <html>
               <head>
                 <title>Vue 3 SSR Example</title>
               </head>
               <body>
-                <div id="app">${await super.View('index.vue', {count: 1})}</div>
+                <div id="app">${await super.View('index.vue', { count: 1 })}</div>
               </body>
             </html>`,
-            status: 200
-        }
+      status: 200
     }
+  }
 }
--- views/index.vue
+++ views/index.vue
@@ -1,1 +1,12 @@
-<div @click="count++">{{ count }}</div>
+<!--
+ * @Author: zhangyu
+ * @Date: 2023-10-28 15:11:14
+ * @LastEditTime: 2023-10-28 16:14:26
+-->
+<template>
+    <div @click="count++">{{ count }}</div>
+</template>
+
+<script></script>
+
+<style></style>
