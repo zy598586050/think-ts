656544efcf69115988cf46b4507068086741684c
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2023-11-10 20:08:29 +0800

    feature:【阶段提交】

--- controller/hello.ts
+++ controller/hello.ts
@@ -1,13 +1,14 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-24 10:04:07
- * @LastEditTime: 2023-11-10 17:06:00
+ * @LastEditTime: 2023-11-10 20:07:21
  */
-import { Context, Controller, ShowSuccess, GetParams } from 'think-ts-lib'
+import { Context, Controller, ShowSuccess, GetParams, ApiException } from 'think-ts-lib'
 
 export default class HelloController extends Controller {
   say(ctx: Context) {
-    GetParams(ctx, true)
+    // GetParams(ctx, true, 'sdf')
+    ApiException()
     return ShowSuccess([])
   }
 }
--- route/hello.ts
+++ route/hello.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-18 10:49:20
- * @LastEditTime: 2023-11-10 19:22:27
+ * @LastEditTime: 2023-11-10 19:49:20
  */
 import { RouteType, Context, ExceptionType } from 'think-ts-lib'
 
@@ -9,6 +9,6 @@ export default (route: RouteType) => {
     //route.post('/hello', 'hello/say')
     //route.get('/hello', 'sdf')
     route.group('api/v1', (routes: RouteType) => {
-        routes.post('hello', 'hello/say')
+        routes.delete('hello', 'hello/say')
     })
 }
