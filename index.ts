8ade56ffcb08e705dc590531e8ff16dd3c4d08bb
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2024-01-05 13:22:24 +0800

    feature:【部署】

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-17 11:15:17
- * @LastEditTime: 2023-11-07 17:13:10
+ * @LastEditTime: 2024-01-05 13:21:59
  */
 
 // 入口文件
 import ThinkTS from 'think-ts-lib'
 
 ThinkTS.start()
+
+process.exit(0)
